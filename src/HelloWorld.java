294d53a365fe052b81737dbd621213f2d29853c1
Administrator <dyx1176062304@163.com>
2019-05-31 18:18:08 +0800

    HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("HelloWorld  dyx 哈哈哈");
+        System.out.println("HelloWorld  dyx");
     }
 }
